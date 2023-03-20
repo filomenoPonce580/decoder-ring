03881554fe1ffc032b07599b48e1bfeecbdb3ffa
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-03-20 14:12:08 -0700

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -7,30 +7,29 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
   
   //create alphabet variable. 2 iterations. positive shift uses first iteration[0-25], negative shift uses second[26-51]
-  const alphabet ="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+  const alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
 
   //input: word being decifered of encrypted
   //shift: shift in letters
   //encode: true means encrypt, false means decifer
   function caesar(input, shift, encode = true) {
-      //terminate early if shift is invalid
-  if(!shift || shift > 25 || shift < -25 || shift === 0) return false
+    //terminate early if shift is invalid
+    if(!shift || shift > 25 || shift < -25 || shift === 0) return false
 
-    //mutate alphabet and input into arrays
-      //&input to lowercase
+    //mutate alphabet and input into arrays (&input to lowercase)
     let splitInput = input.toLowerCase().split("")
     let alphabetArray = alphabet.split("")
 
     //create result
     let result
 
-    //if encode = false, flip shift
+    //if encode is false, flip shift
     if (encode === false){
       shift = shift *-1
     }
 
     //if statements
-    //+shift, search by first indexOf and shift right
+    //positive shift, search by first indexOf and shift right
     if(shift > 0){
       result = splitInput.map((letter)=>{
         //account for spaces & special characters
@@ -40,7 +39,7 @@ const caesarModule = (function () {
           return alphabetArray[alphabetArray.indexOf(letter) + shift]
         }
       })
-    //-shift, search by lastIndexOf and shift left
+    //negative shift(), search by lastIndexOf and shift left
     }else{
       result = splitInput.map((letter)=>{
         if(letter.match(/[^a-z]/)){
