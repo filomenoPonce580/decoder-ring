7a9d737d8010bf29b2ae7ce9241e89190a6560ce
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-03-21 10:03:25 -0700

    complete and functional

--- src/substitution.js
+++ src/substitution.js
@@ -4,17 +4,27 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-  const actualAlphabet = "";
+  const actualAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    //guard clause: aplhabet is 26 characters & all characters unique
+    if(!alphabet || alphabet.length !== 26 || new Set(alphabet).size !== alphabet.length) return false
+
+    //remap the input and modify based on incoming alphabet compared to actual alphabet
+    return input
+      .toLowerCase()
+      .split('')
+      //letter->actual index -> same index on new alphabet -> value
+      .map((character)=>{
+        if (character.match(" ")) return character; //keeps spaces
+        if(encode){
+          return alphabet[actualAlphabet.indexOf(character)]
+        }else{
+          return actualAlphabet[alphabet.indexOf(character)]
+        }
+      })
+      .join('')
   }
-
-
-  //from rob
-  substitution("thinkful", "fake26characteralphabet")
-    //t's actual position is replaced with same index in fake alphabet
 
   return {
     substitution,
@@ -22,3 +32,21 @@ const substitutionModule = (function () {
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
+
+
+/*
+TESTS
+console.log(substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev")); //> 'jrufscpw'
+console.log(substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev")); //> 'elp xhm xf mbymwwmfj dne'
+console.log(substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false)); //> 'thinkful'
+
+        console.log("**********************")
+
+console.log(substitution("message", "$wae&zrdxtfcygvuhbijnokmpl")); //> "y&ii$r&"
+console.log(substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false)); //> "message"
+
+          console.log("**********************")
+
+console.log(substitution("thinkful", "short")); //> false
+console.log(substitution("thinkful", "abcabcabcabcabcabcabcabcyz")); //> false
+*/
